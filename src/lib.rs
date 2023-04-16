08d710a47e8063eb213de4a44b5e38ce004aac81
BuildTools <unconfigured@null.spigotmc.org>
2023-04-16 19:00:08 +0200

    Added lib.rs to access this from rust

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate nom;
+extern crate itertools;
+
+#[macro_use]
+mod persistent_list_map;
+#[macro_use]
+mod persistent_list;
+#[macro_use]
+mod protocol;
+#[macro_use]
+mod symbol;
+#[macro_use]
+mod var;
+mod clojure_std;
+mod clojure_string;
+mod environment;
+mod error_message;
+mod ifn;
+mod iterable;
+mod keyword;
+mod lambda;
+mod maps;
+mod namespace;
+mod persistent_vector;
+mod protocols;
+mod reader;
+mod repl;
+mod rust_core;
+mod traits;
+mod type_tag;
+mod user_action;
+mod util;
+mod value;
